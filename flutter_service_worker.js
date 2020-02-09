20ceefef9983075b5ee8a5004da27a5fe5cc5367
adityapstar <adityap.star@gmail.com>
2020-02-09 09:32:14 +0000

    Deploying to gh-pages from master - 711873adca846750e7396dee2fcf4500f02032f7 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,9 +6,10 @@ const RESOURCES = {
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/AssetManifest.json": "30823eeecf34b4c7bcef33308cacd19e",
+"/assets/AssetManifest.json": "c8c96be442923132ae406dd6e4b38832",
 "/assets/LICENSE": "28eaf584c7f90ed893cff7f4c06156f7",
 "/assets/images/rahbert.png": "98e5b6c34fa8cd40d5ada1e3deacb58d",
+"/assets/images/app_icon.png": "5f5244069ceba8559afa0a04f02321ef",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/main.dart.js": "f9a0fdf8371ed9f8da42666aef89a01d"
