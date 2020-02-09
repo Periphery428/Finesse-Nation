38b3bdfd8c0170089134d499202083642b7a7201
adityapstar <adityap.star@gmail.com>
2020-02-09 04:41:00 +0000

    Deploying to gh-pages from master - 2d34ddd8a300d6647abcdd51cf30434dd1b9258c 🚀

---
+++ flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "f4e9d07268c4959f6f64e425712ec2e1",
+"/manifest.json": "e635fd6dd182a2cf920db16d047ea5f3",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/AssetManifest.json": "30823eeecf34b4c7bcef33308cacd19e",
+"/assets/LICENSE": "dfeb952b6bd9db9ae50aafef86cf5ce5",
+"/assets/images/rahbert.png": "98e5b6c34fa8cd40d5ada1e3deacb58d",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/main.dart.js": "f9a0fdf8371ed9f8da42666aef89a01d"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
