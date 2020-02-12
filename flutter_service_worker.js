2a26caee8f77f85fad92768bd6f565277c786623
Rjbeckwith55 <rjbeckwith55@gmail.com>
2020-02-12 01:25:11 +0000

    Deploying to gh-pages from master - dc1d4ac6f588896950d817fbbcfe8da52c67a9b8 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,12 +7,12 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/AssetManifest.json": "c8c96be442923132ae406dd6e4b38832",
-"/assets/LICENSE": "28eaf584c7f90ed893cff7f4c06156f7",
+"/assets/LICENSE": "fff2a36afe9bed9f2b999ab884332819",
 "/assets/images/rahbert.png": "98e5b6c34fa8cd40d5ada1e3deacb58d",
 "/assets/images/app_icon.png": "5f5244069ceba8559afa0a04f02321ef",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/main.dart.js": "37e08b62221cfbf2baddac066215cadf"
+"/main.dart.js": "c10cd2fa6a27bbaeb01c15ef317b8b1e"
 };
 
 self.addEventListener('activate', function (event) {
@@ -34,9 +34,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
