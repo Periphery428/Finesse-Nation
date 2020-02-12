6887cb875c482fa64a2935abea558e09028c997c
KrastanD <krastan.kdd@gmail.com>
2020-02-11 22:08:06 -0600

    created infinite rahberts

--- lib/main.dart
+++ lib/main.dart
@@ -91,61 +91,97 @@ class _MyHomePageState extends State<MyHomePage> {
         icon: Icon(Icons.add),
         backgroundColor: Colors.pink,
       ),
-      body: new Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.lightBlue, Colors.lightGreen],
-        )),
-        child: new Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GestureDetector(
-                  key: Key('rahbert'),
-                  onTap: _incrementCounter,
-                  child: Image.asset(
-                    'images/rahbert.png',
-                    key: Key('rahbertImage'),
-                    scale: _scale,
-                    height: 470,
-                    width: 470,
-                  )),
-              Text(
-                'You have tapped the image the following number of times:',
-              ),
-              Text('$_counter',
-                  key: Key('counterKey'),
-                  style: TextStyle(
-                      fontSize: 60,
-                      foreground: Paint()
-                        ..shader = new LinearGradient(
-                                colors: [Colors.red, Colors.blue])
-                            .createShader(Rect.fromLTWH(0, 0, 400, 0)))),
-            ],
-          ),
-        ),
-      ),
+      body: _buildFinesseList(),
 
       // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    }
+
+    List<Finesse> _finesses = new List();
+    getFinesses() {
+      _finesses.add(Finesse("Fin 1 Title", "Fin 1 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 2 Title", "Fin 2 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 3 Title", "Fin 3 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 4 Title", "Fin 4 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 5 Title", "Fin 5 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 6 Title", "Fin 6 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 7 Title", "Fin 7 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 8 Title", "Fin 8 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 9 Title", "Fin 9 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 10 Title", "Fin 10 Duration", "images/rahbert.png"));
+      _finesses.add(Finesse("Fin 11 Title", "Fin 11 Duration", "images/rahbert.png"));
+    }
+
+
+    Widget _buildFinesseList() {
+      return new Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+            colors: [Colors.lightBlue, Colors.lightGreen],
+          )
+        ),
+        child: new Center(
+
+          child:
+            ListView.builder(
+              itemBuilder: (context, i) {
+                if(i == 0){
+                  getFinesses();
+                }
+                if (i.isOdd) return Divider();
+                final index = i ~/ 2;
+                if(index >= _finesses.length){
+                  _finesses.addAll(_finesses.take(10));
+                }
+                return _buildFinesseCard(_finesses[index]);
+              })
+        )
+      );
+    }
+
+
+  Widget _buildFinesseCard(Finesse fin) {
+    return Card(
+      color: Colors.lime[200],
+      child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+             Image.asset(fin.getImage()),
+             ListTile(
+              leading: Icon(Icons.accessible_forward),
+              title: Text(fin.getTitle()),
+              subtitle: Text(fin.getBody()),
+            ),
+          ]),
     );
   }
 }
+
+
+class Finesse{
+  String title;
+  String body;
+  String duration;
+  String image;
+
+  Finesse(String title, String body, String img){
+    this.title = title;
+    this.body = body;
+    this.image = img;
+  }
+
+  String getImage(){
+    return image;
+  }
+
+  String getBody(){
+    return body;
+  }
+
+  String getTitle(){
+    return image;
+  }
+}
+
