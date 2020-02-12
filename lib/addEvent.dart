ae6346d3a5b7b249a2c9fb5b6f0e05314f789962
Robert Beckwith <robertb4@illinois.edu>
2020-02-11 20:28:07 -0600

    added test post command

--- lib/addEvent.dart
+++ lib/addEvent.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class addEvent extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddEvent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appTitle = 'Share Free Stuff';
@@ -24,6 +27,40 @@ class MyCustomForm extends StatefulWidget {
     return MyCustomFormState();
   }
 }
+class Post {
+  final String eventName;
+  final String location;
+  final String image;
+
+  Post({this.eventName, this.location, this.image});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      eventName: json['eventName'],
+      location: json['location'],
+      image: json['image'],
+    );
+  }
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["eventName"] = eventName;
+    map["location"] = location;
+    map["image"] = image;
+
+    return map;
+  }
+}
+
+Future<Post> createPost(String url, {Map body}) async {
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
 
 // Create a corresponding State class.
 // This class holds data related to the form.
@@ -37,6 +74,7 @@ class MyCustomFormState extends State<MyCustomForm> {
 
   @override
   Widget build(BuildContext context) {
+    final CREATE_POST_URL = 'https://ptsv2.com/t/yofb8-1581474248/post';
     // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
@@ -54,13 +92,19 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: RaisedButton(
-              onPressed: () {
+              color: Colors.blue,
+              onPressed: () async {
                 // Validate returns true if the form is valid, or false
                 // otherwise.
                 if (_formKey.currentState.validate()) {
                   // If the form is valid, display a Snackbar.
                   Scaffold.of(context)
                       .showSnackBar(SnackBar(content: Text('Processing Data')));
+
+                  Post newPost = new Post(
+                      eventName: "name", location: "90.00000", image: "test");
+                  Post p = await createPost(CREATE_POST_URL,
+                      body: newPost.toMap());
                 }
               },
               child: Text('Submit'),
@@ -71,3 +115,4 @@ class MyCustomFormState extends State<MyCustomForm> {
     );
   }
 }
+
