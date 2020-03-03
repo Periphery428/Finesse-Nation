5fc63b02167127dd768682b4dddea7e8f2e6c913
adityapstar <adityap.star@gmail.com>
2020-03-03 20:49:13 +0000

    Deploying to gh-pages from master - c332afdaaccf692ec2992efff16380e892b42e38 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,19 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/LICENSE": "fff2a36afe9bed9f2b999ab884332819",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+  "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/main.dart.js": "1ce27c2e670fc70d62d8d64529075b8b",
+"/manifest.json": "e635fd6dd182a2cf920db16d047ea5f3",
+"/index.html": "f4e9d07268c4959f6f64e425712ec2e1",
+"/assets/images/bemzo.jpg": "570492a60b67ca635d4c5015d4f8a5b3",
 "/assets/images/app_icon.png": "5f5244069ceba8559afa0a04f02321ef",
 "/assets/images/rahbert.png": "98e5b6c34fa8cd40d5ada1e3deacb58d",
-"/assets/images/bemzo.jpg": "570492a60b67ca635d4c5015d4f8a5b3",
+"/assets/images/remram.png": "b5b8ffc328d2b85fd373a8b6e0e5a909",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/AssetManifest.json": "90f2fa0daf400205cf17a106f261bfb2",
-"/index.html": "f4e9d07268c4959f6f64e425712ec2e1",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/main.dart.js": "20b22d992cbac9f9fb4ce735845a715f",
-"/manifest.json": "e635fd6dd182a2cf920db16d047ea5f3"
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/LICENSE": "cae46c075412c628ddfc083c67a7cf9b",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/AssetManifest.json": "1488b1c8c65b7dfa45dacdb9ea306512"
 };
 
 self.addEventListener('activate', function (event) {
